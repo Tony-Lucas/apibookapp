b0064e7fa0c03e16832af23b1784cfab3919939b
Tony Lucas <tonyjogos123@gmail.com>
2021-01-23 13:23:35 -0300

    asdasdasd

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('dbbookapp', 'root', '55425610a', {
-    host: 'localhost',
+const sequelize = new Sequelize('heroku_24faacd44fbd71c', 'b4941ba3b94230', '454277a1', {
+    host: 'us-cdbr-east-03.cleardb.com',
     dialect: 'mysql',
     raw: true,
     define: {
