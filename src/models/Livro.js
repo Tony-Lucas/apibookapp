e5a4b67ae737de91466dc749a8bbb6f171f3e6d4
Tony Lucas <tonyjogos123@gmail.com>
2021-01-24 14:24:27 -0300

    asdasd

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -4,7 +4,8 @@ const sequelize = require("../config/database");
 const Livro = sequelize.define("livros", {
     nome: { type: Sequelize.STRING, allowNull: false },
     categoria: { type: Sequelize.STRING, allowNull: false },
-    autor: { type: Sequelize.STRING, allowNull: false }
+    autor: { type: Sequelize.STRING, allowNull: false },
+    linkThumbnail: { type: Sequelize.STRING, allowNull: false }
 })
 
 module.exports = Livro
--- src/routes/livroRoute.js
+++ src/routes/livroRoute.js
@@ -14,7 +14,7 @@ Router.get("/:id", authentication, async (req, res) => {
 })
 
 Router.post("/", authentication, async (req, res) => {
-    const livro = await Livro.create({ nome: req.body.nome, categoria: req.body.categoria, autor: req.body.autor, userId: req.body.userId });
+    const livro = await Livro.create({ nome: req.body.nome, categoria: req.body.categoria, autor: req.body.autor, userId: req.body.userId,linkThumbnail:req.body.linkThumbnail });
     if (livro) {
         res.json({ success: true })
     } else {
@@ -23,7 +23,7 @@ Router.post("/", authentication, async (req, res) => {
 })
 
 Router.post("/altera", authentication, async (req, res) => {
-    const livro = await Livro.update({ nome: req.body.nome, categoria: req.body.categoria, autor: req.body.autor, userId: req.body.userId },{where:{id:req.body.id}});
+    const livro = await Livro.update({ nome: req.body.nome, categoria: req.body.categoria, autor: req.body.autor, userId: req.body.userId,linkThumbnail: req.body.linkThumbnail},{where:{id:req.body.id}});
     if (livro) {
         res.json({ success: true })
     } else {
