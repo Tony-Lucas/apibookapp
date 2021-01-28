f9a144dcda22d811adf518baff2e7c6dfd22ece6
Tony Lucas <tonyjogos123@gmail.com>
2021-01-28 12:55:41 -0300

    asdasda

--- src/models/Solicitacao.js
+++ src/models/Solicitacao.js
@@ -3,7 +3,6 @@ const sequelize = require("../config/database");
 
 
 const Solicitacao = sequelize.define("solicitacoes", {
-    tipo:{type:Sequelize.STRING,allowNull: false},
     aceito:{type:Sequelize.BOOLEAN,allowNull: false},
     visualizado:{type:Sequelize.BOOLEAN,defaultValue:false}
 })
