c722625e458d6e938b5bdbbd79b1ab7c56e883d8
Tony Lucas <tonyjogos123@gmail.com>
2021-01-29 13:09:17 -0300

    asdasda

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -9,7 +9,7 @@ const { path } = require("../config/server");
 
 const storage = multer.diskStorage({
     destination: function (req,file,cb){
-        cb(null,path.join(__dirname + '/public/uploads'))
+        cb(null,'./public/uploads')
     },
     filename:function (req,file,cb){
         cb(null,file.originalname + "-" + Date.now())
