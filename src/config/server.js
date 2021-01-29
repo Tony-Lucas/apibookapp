f142e9507795da0487f07aae956e9547412912cf
Tony Lucas <tonyjogos123@gmail.com>
2021-01-29 13:16:20 -0300

    asdasda

--- src/config/server.js
+++ src/config/server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 require("../models/associations");
 const cors = require("cors");
 
+app.use(express.static("uploads"))
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
